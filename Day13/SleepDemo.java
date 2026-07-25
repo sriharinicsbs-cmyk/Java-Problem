882a48f710410499b85f6b3e5da66255c183774c
sriharinicsbs-cmyk/ <sriharinicsbs@gmail.com>
2026-07-25 15:48:29 +0530

    Initial

---
+++ Day13/SleepDemo.java
@@ -0,0 +1,15 @@
+package Day13;
+public class SleepDemo{
+
+    public static void main(String[] args) throws Exception{
+
+        for(int i=1;i<=5;i++){
+
+            System.out.println(i);
+
+            Thread.sleep(1000);
+        }
+
+        System.out.println("Completed");
+    }
+}
---
+++ Day13/ThrowDemo.java
@@ -0,0 +1,16 @@
+package Day13;
+
+public class ThrowDemo {
+
+    public static void main(String[] args) {
+
+        int age = 15;
+
+        if(age < 18){
+
+            throw new ArithmeticException("Not Eligible to Vote");
+        }
+
+        System.out.println("Eligible");
+    }
+}
